b16c8864f3ff12c6821be0c3f8bd0898c02be0fe
nate <clevelandsun@gmail.com>
2021-05-26 14:55:07 -0400

    game_test.go comment out

--- Learn-go-with-tests/BuilingAnApp/game_test.go
+++ Learn-go-with-tests/BuilingAnApp/game_test.go
@@ -1,60 +1,60 @@
-package poker_test
+// package poker_test
 
-import (
-	"testing"
-	"time"
+// import (
+// 	"testing"
+// 	"time"
 
-	poker "github.com/FreeCodeUserJack/MasteringGo/Learn-go-with-tests/BuilingAnApp"
-)
+// 	poker "github.com/FreeCodeUserJack/MasteringGo/Learn-go-with-tests/BuilingAnApp"
+// )
 
 
-func TestGame_Start(t *testing.T) {
-	t.Run("schedules alerts on game start for 5 players", func(t *testing.T) {
-		blindAlerter := &poker.SpyBlindAlerter{}
-		game := poker.NewGame(blindAlerter, dummyPlayerStore)
+// func TestGame_Start(t *testing.T) {
+// 	t.Run("schedules alerts on game start for 5 players", func(t *testing.T) {
+// 		blindAlerter := &poker.SpyBlindAlerter{}
+// 		game := poker.NewGame(blindAlerter, dummyPlayerStore)
 
-		game.Start(5)
+// 		game.Start(5)
 
-		cases := []poker.ScheduledAlert{
-			{At: 0 * time.Second, Amount: 100},
-			{At: 10 * time.Minute, Amount: 200},
-			{At: 20 * time.Minute, Amount: 300},
-			{At: 30 * time.Minute, Amount: 400},
-			{At: 40 * time.Minute, Amount: 500},
-			{At: 50 * time.Minute, Amount: 600},
-			{At: 60 * time.Minute, Amount: 800},
-			{At: 70 * time.Minute, Amount: 1000},
-			{At: 80 * time.Minute, Amount: 2000},
-			{At: 90 * time.Minute, Amount: 4000},
-			{At: 100 * time.Minute, Amount: 8000},
-		}
+// 		cases := []poker.ScheduledAlert{
+// 			{At: 0 * time.Second, Amount: 100},
+// 			{At: 10 * time.Minute, Amount: 200},
+// 			{At: 20 * time.Minute, Amount: 300},
+// 			{At: 30 * time.Minute, Amount: 400},
+// 			{At: 40 * time.Minute, Amount: 500},
+// 			{At: 50 * time.Minute, Amount: 600},
+// 			{At: 60 * time.Minute, Amount: 800},
+// 			{At: 70 * time.Minute, Amount: 1000},
+// 			{At: 80 * time.Minute, Amount: 2000},
+// 			{At: 90 * time.Minute, Amount: 4000},
+// 			{At: 100 * time.Minute, Amount: 8000},
+// 		}
 
-		checkSchedulingCases(cases, t, blindAlerter)
-	})
+// 		checkSchedulingCases(cases, t, blindAlerter)
+// 	})
 
-	t.Run("schedules alerts on game start for 7 players", func(t *testing.T) {
-		blindAlerter := &poker.SpyBlindAlerter{}
-		game := poker.NewGame(blindAlerter, dummyPlayerStore)
+// 	t.Run("schedules alerts on game start for 7 players", func(t *testing.T) {
+// 		blindAlerter := &poker.SpyBlindAlerter{}
+// 		game := poker.NewGame(blindAlerter, dummyPlayerStore)
 
-		game.Start(7)
+// 		game.Start(7)
 
-		cases := []poker.ScheduledAlert{
-			{At: 0 * time.Second, Amount: 100},
-			{At: 12 * time.Minute, Amount: 200},
-			{At: 24 * time.Minute, Amount: 300},
-			{At: 36 * time.Minute, Amount: 400},
-		}
+// 		cases := []poker.ScheduledAlert{
+// 			{At: 0 * time.Second, Amount: 100},
+// 			{At: 12 * time.Minute, Amount: 200},
+// 			{At: 24 * time.Minute, Amount: 300},
+// 			{At: 36 * time.Minute, Amount: 400},
+// 		}
 
-		checkSchedulingCases(cases, t, blindAlerter)
-	})
+// 		checkSchedulingCases(cases, t, blindAlerter)
+// 	})
 
-}
+// }
 
-func TestGame_Finish(t *testing.T) {
-	store := &poker.StubPlayerStore{}
-	game := poker.NewGame(dummyBlindAlerter, store)
-	winner := "Ruth"
+// func TestGame_Finish(t *testing.T) {
+// 	store := &poker.StubPlayerStore{}
+// 	game := poker.NewGame(dummyBlindAlerter, store)
+// 	winner := "Ruth"
 
-	game.Finish(winner)
-	poker.AssertPlayerWin(t, store, winner)
-}
+// 	game.Finish(winner)
+// 	poker.AssertPlayerWin(t, store, winner)
+// }
